7722f1f458a0c16a1e3cc5e4f37ee1fb074d44e5
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2025-10-07 00:49:10 +0100

    added middleware for deleting collections

--- backend/src/contollers/collection-controller.ts
+++ backend/src/contollers/collection-controller.ts
@@ -3,38 +3,132 @@ import asyncErrorHandler from "../utils/async-error-handler";
 import CustomError from "../utils/custom-error";
 import Collections from "../models/collection-model";
 
-export const createCollection = asyncErrorHandler(async(req: Request, res: Response, next: NextFunction) => {
-	const collection = await Collections.create(req.body)
-	if(!collection) {
-		const error = new CustomError(500, `Failed to create new collection. Try again later.`)
-		return next(error)
-	}
-	res.status(201).json({
-		status: "OK"
-	})
-})
+export const createCollection = asyncErrorHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const collection = await Collections.create(req.body);
+    if (!collection) {
+      const error = new CustomError(
+        500,
+        `Failed to create new collection. Try again later.`
+      );
+      return next(error);
+    }
+    res.status(201).json({
+      status: "OK",
+    });
+  }
+);
 
-export const addArticlesToCollection = asyncErrorHandler(async(req: Request, res: Response, next: NextFunction) => {
-	const name = req.body.collection
-	const urls = req.body.urls
-	
-	const addedArticles = await Collections.updateOne({name}, {$push: {urls: {each: urls}}})
-	if(!addedArticles) {
-		const error = new CustomError(500, `Failed to add ${urls.length === 1 ? "article" : "articles"}. Try again later.`)
-		return next(error)
-	}
-	
-	res.status(200).json({
-		status: "OK"
-	})
-})
+export const addArticlesToCollection = asyncErrorHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const name = req.body.name;
+    const urls = req.body.urls;
 
-export const editCollection = asyncErrorHandler(async(req: Request, res: Response, next: NextFunction) => {
-	const name = req.body.collection
+    const addedArticles = await Collections.updateOne(
+      { name },
+      { $push: { urls: { each: urls } } }
+    );
+    if (!addedArticles) {
+      const error = new CustomError(
+        500,
+        `Failed to add ${
+          urls.length === 1 ? "article" : "articles"
+        }. Try again later.`
+      );
+      return next(error);
+    }
 
-})
+    res.status(200).json({
+      status: "OK",
+    });
+  }
+);
 
+export const editCollection = asyncErrorHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const name = req.body.name;
+    const newName = req.body?.newName;
+    const newDescription = req.body?.newDescription;
+    const removedURL = req.body?.url;
 
-export const deleteCollection = asyncErrorHandler(async(req: Request, res: Response, next: NextFunction) => {
+    if (newName && newDescription) {
+      const editedCollection = await Collections.updateOne(
+        { name },
+        { $set: { name: newName, description: newDescription } }
+      );
+      if (!editedCollection) {
+        const error = new CustomError(
+          500,
+          "Failed to change either of the name and the desription of the collection. Try again later."
+        );
+        return next(error);
+      }
+    } else {
+      if (newName) {
+        const editedCollection = await Collections.updateOne(
+          { name },
+          { $set: { name: newName } }
+        );
+        if (!editedCollection) {
+          const error = new CustomError(
+            500,
+            "Failed to change the name of the collection. Try again later."
+          );
+          return next(error);
+        }
+      }
+      if (newDescription) {
+        const editedCollection = await Collections.updateOne(
+          { name },
+          { $set: { description: newDescription } }
+        );
+        if (!editedCollection) {
+          const error = new CustomError(
+            500,
+            "Failed to change the description of the collection. Try again later."
+          );
+          return next(error);
+        }
+      }
+    }
 
-})
+    if (removedURL) {
+      const editedCollection = await Collections.updateOne(
+        { name },
+        { $pull: { urls: removedURL } }
+      );
+      if (!editedCollection) {
+        const error = new CustomError(
+          500,
+          "Failed to remove article from collection. Try again later."
+        );
+        return next(error);
+      }
+    }
+
+    res.status(200).json({
+      status: "OK",
+    });
+  }
+);
+
+export const deleteCollection = asyncErrorHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const name = req.body.Name;
+
+    const deletedCollection = await Collections.deleteOne({ name });
+    if (!deletedCollection) {
+      const error = new CustomError(
+        500,
+        "Failed to delete collection. Try again later."
+      );
+      return next(error);
+    }
+
+    res.status(204).json({
+      status: "OK",
+    });
+  }
+);
+
+// "Failed to change the name and the decription of the collection. Try again later"
