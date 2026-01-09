c046bd86928cef550b136189daa4d70d19a5c902
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2026-01-10 00:24:04 +0100

    added a really nice animation effect to link-helper

--- frontend/src/routes/_header-layout/index.tsx
+++ frontend/src/routes/_header-layout/index.tsx
@@ -1,7 +1,5 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import ArticleCard from "@/components/article-card";
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
 import RecentCard from "@/components/recent-card";
 import { useQuery } from "@tanstack/react-query";
 import { ChevronRight } from "lucide-react";
@@ -18,6 +16,18 @@ interface Recent {
   siteName: string;
   title: string;
   url: string;
+}
+interface Article {
+  _id: string;
+  excerpt: string;
+  image: string;
+  siteName: string;
+  title: string;
+  url: string;
+}
+interface Feed {
+  category: string;
+  data: Article[];
 }
 
 export const fetchHomeFeed = async () => {
@@ -41,11 +51,11 @@ function App() {
     queryFn: fetchHomeFeed,
     staleTime: Infinity,
   });
-	
+
   console.log("data:", data);
   const user = data?.data.user;
-  const articles = data?.data.articles;
-  console.log("articles:", articles);
+  const feeds: Feed[] | null = data?.data.articles;
+  console.log("articles:", feeds);
   const recents: Array<Recent[]> | null = data?.data.recents;
   console.log("recents:", recents);
   const time = new Date().getHours();
@@ -74,42 +84,55 @@ function App() {
           ) : (
             <div>
               <div className="mb-1 mx-4 flex justify-between items-baseline">
-                <h1 className="font-medium text-base">Recent Saves</h1>
-                <LinkHelper label="Go to Saves" to="/saves" width="w-[83%]" textSize="text-sm" icon={<ChevronRight className="w-4.5! h-4.5!" />}/>
+                <h3 className="font-medium text-base">Recent Saves</h3>
+                <LinkHelper
+                  label="Go to Saves"
+                  to="/saves"
+                  textSize="text-sm"
+                  icon={<ChevronRight className="h-4.5! max-w-0 group-hover:max-w-4.5 text-primary -mb-[3.5px] opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out pointer-events-none group-hover:pointer-events-auto" />}
+                />
               </div>
               <div className="mb-4 w-screen flex shrink-0 flex-nowrap overflow-x-auto">
                 {recents.map((recent: Recent[]) => {
-									const unit = recent[0]
-									const {_id, image, siteName, title, url} = unit
-									return (
-										<div
-										  key={url}
-										  className="min-w-[84%] first:ml-4 first:mr-2 ml-2 mr-2 last:ml-2 last:mr-4"
-										>
-										  <RecentCard
-											key={_id}
-										    _id={_id}
-										    image={image}
-										    siteName={siteName}
-										    title={title}
-										    url={url}
-										  />
-										</div>
-									)
-								}
-								)}
+                  const unit = recent[0];
+                  const { _id, image, siteName, title, url } = unit;
+                  return (
+                    <div
+                      key={_id}
+                      className="min-w-[84%] first:ml-4 first:mr-2 ml-2 mr-2 last:ml-2 last:mr-4"
+                    >
+                      <RecentCard
+                        _id={_id}
+                        image={image}
+                        siteName={siteName}
+                        title={title}
+                        url={url}
+                      />
+                    </div>
+                  );
+                })}
               </div>
             </div>
           )}
         </div>
         <div className="mx-4">
           <div className="mb-4">
-            <h1 className="font-medium text-xl">
-              Popular Topics
-            </h1>
+            <h2 className="font-medium text-xl">Popular Topics</h2>
           </div>
           <div className="flex flex-col gap-6">
-            {/* <ArticleCard /> */}
+            {!feeds
+              ? ""
+              : feeds.map((feed: Feed) => {
+                  const { category, data } = feed;
+                  return (
+                    <div>
+                      <div className="flex justify-between items-baseline">
+												<h3 className="font-medium text-lg">{category}</h3>
+												<LinkHelper to="/" label={`Go to ${category}`} textSize="text-sm"/>
+											</div>
+                    </div>
+                  );
+                })}
           </div>
         </div>
       </section>
