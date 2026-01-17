4bb4a89e9e72ba3cf4b227c8a1854ca24ef11e16
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2026-01-17 04:30:10 +0100

    daily check-in: 17-01-2026

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -52,3 +52,14 @@ export const textLowerCasifierAndHyphenator = (text: string): string => {
   text = textArray.join("-");
   return text;
 };
+
+export const textCapitalizerAndSpacifier = (text: string): string => {
+  const textArray = text.split("-");
+	const capitalizedTextArray = []
+  for (let i = 0; i < textArray.length; i++) {
+		const capitalizedText = `${textArray[i].charAt(0).toUpperCase()}${textArray[i].substring(1)}`
+		capitalizedTextArray.push(capitalizedText)
+  }
+  text = capitalizedTextArray.join(" ");
+  return text;
+};
