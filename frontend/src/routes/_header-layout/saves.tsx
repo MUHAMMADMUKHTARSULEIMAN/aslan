b530b99e6273fed245162830f7ec3137107df6ec
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2026-02-13 09:23:16 +0100

    added icons to the dropdown menu options

--- frontend/src/routes/_header-layout/saves.tsx
+++ frontend/src/routes/_header-layout/saves.tsx
@@ -13,7 +13,7 @@ import {
   useLocation,
 } from "@tanstack/react-router";
 import { useAtom } from "jotai";
-import { ChevronDown } from "lucide-react";
+import { Archive, ChevronDown, Files, Star } from "lucide-react";
 import { useEffect } from "react";
 
 export const Route = createFileRoute("/_header-layout/saves")({
@@ -34,29 +34,32 @@ function RouteComponent() {
 
   return (
     <div className="mb-12">
-      <div ref={trackingRef} className="w-full bg-background fixed top-0 translate-y-[57.53px] left-0 z-40 transition-transform duration-350 ease-in-out data-[visible=true]:translate-y-[57.53px] data-[visible=false]:translate-y-0">
+      <div ref={trackingRef} className="w-full bg-background fixed top-0 translate-y-[53.53px] left-0 z-40 transition-transform duration-250 ease-in-out data-[visible=true]:translate-y-[53.53px] data-[visible=false]:translate-y-0">
         <div className="flex items-center gap-1 my-2 mx-4">
           <h2 className="font-medium text-xl">{currentRoute}</h2>
           <DropdownMenu open={open} onOpenChange={setOpen}>
             <DropdownMenuTrigger asChild>
               <ChevronDown
                 data-open={open}
-                className="cursor-pointer data-[open=true]:rotate-180 rounded-sm hover:bg-foreground/10 transition-transform duration-100 ease-linear"
+                className="cursor-pointer data-[open=true]:rotate-180 rounded-sm hover:text-emerald-600  dark:hover:text-emerald-300 transition-transform duration-100 ease-linear"
               />
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="mt-[7.5px] rounded-t-none border-[1.5px] border-t-0">
+            <DropdownMenuContent className="mt-[7px] rounded-t-none border-t-0">
               <Link to="/saves">
-                <DropdownMenuItem className="focus:bg-primary cursor-pointer">
+                <DropdownMenuItem>
+									<Files />
                   Saves
                 </DropdownMenuItem>
               </Link>
               <Link to="/saves/favourites">
-                <DropdownMenuItem className="focus:bg-primary cursor-pointer">
+                <DropdownMenuItem>
+									<Star />
                   Favourites
                 </DropdownMenuItem>
               </Link>
               <Link to="/saves/archives">
-                <DropdownMenuItem className="focus:bg-primary cursor-pointer">
+                <DropdownMenuItem>
+									<Archive />
                   Archives
                 </DropdownMenuItem>
               </Link>
