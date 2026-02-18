360ebf528f190ec00317c4ee8b7e1937ab95f8e9
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2026-02-18 17:17:44 +0100

    first list of tabs  is now directly inside /feeds

--- frontend/src/routes/_header-layout/feeds.tsx
+++ frontend/src/routes/_header-layout/feeds.tsx
@@ -1,5 +1,20 @@
-import { textLowerCasifierAndHyphenator, topics } from "@/lib/utils";
-import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import useScrollTracking from "@/hooks/use-scroll-tracking";
+import {
+  ampersandToAnd,
+  textLowerCasifierAndHyphenator,
+  topics,
+} from "@/lib/utils";
+import { categoryAtom } from "@/store/atoms";
+import {
+  createFileRoute,
+  Link,
+  Outlet,
+  redirect,
+} from "@tanstack/react-router";
+import { useAtom } from "jotai";
+import { useEffect, useState } from "react";
 
 export const Route = createFileRoute("/_header-layout/feeds")({
   component: RouteComponent,
@@ -7,7 +22,9 @@ export const Route = createFileRoute("/_header-layout/feeds")({
     if (location.pathname === "/feeds" || location.pathname === "/feeds/") {
       throw redirect({
         to: "/feeds/$category",
-        params: { category: textLowerCasifierAndHyphenator(topics[0]) },
+        params: {
+          category: ampersandToAnd(textLowerCasifierAndHyphenator(topics[0])),
+        },
         replace: true,
       });
     }
@@ -15,9 +32,54 @@ export const Route = createFileRoute("/_header-layout/feeds")({
 });
 
 function RouteComponent() {
+  const [newCategory, setNewCategory] = useAtom(categoryAtom);
+  const tabRef = useScrollTracking();
+
+  const [scopy, setScopy] = useState(0);
+  useEffect(() => {
+    setScopy(1);
+  }, []);
+
   return (
-	<div>
-		<Outlet />
-	</div>
-	)
+    <div>
+      <Tabs key={scopy} defaultValue={newCategory} className="w-full gap-0">
+        <div
+          ref={tabRef}
+          className="fixed top-0 translate-y-[53.53px] left-0 z-40 transition-transform duration-250 ease-in-out data-[visible=true]:translate-y-[53.53px] data-[visible=false]:-translate-y-[86.53px]"
+        >
+          <ScrollArea className="w-screen whitespace-nowrap border-b-[1.5px] border-border">
+            <TabsList ref={tabRef}>
+              {topics.map((topic: string) => {
+                const normalizedTopic = ampersandToAnd(textLowerCasifierAndHyphenator(topic))
+                return (
+                  <Link
+                    key={normalizedTopic}
+                    to="/feeds/$category"
+                    params={{ category: normalizedTopic }}
+                  >
+                    <TabsTrigger
+                      key={normalizedTopic}
+                      value={normalizedTopic}
+                      onClick={() => setNewCategory(normalizedTopic)}
+                    >
+                      {topic}
+                    </TabsTrigger>
+                  </Link>
+                );
+              })}
+            </TabsList>
+            <ScrollBar orientation="horizontal" className="" />
+          </ScrollArea>
+        </div>
+        {topics.map((topic: string) => {
+          const normalizedTopic = ampersandToAnd(textLowerCasifierAndHyphenator(topic));
+          return (
+            <TabsContent key={normalizedTopic} value={normalizedTopic}>
+              <Outlet />
+            </TabsContent>
+          );
+        })}
+      </Tabs>
+    </div>
+  );
 }
